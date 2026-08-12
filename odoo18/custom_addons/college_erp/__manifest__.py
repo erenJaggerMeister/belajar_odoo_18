d90b9853a80a57a76bb2ef5548cf64832a1fd9d6
erenJaggerMeister <marcelliusfelixmatius@gmail.com>
2026-08-12 22:42:34 +0700

    Fix gitignore to track only custom addons

---
+++ odoo18/custom_addons/college_erp/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "College ERP",
+    'version': "18.0.1.1",
+    'license': "LGPL-3",
+    'summary': """An erp for college education""",
+    'description': """From students administration to exam , this covers all aspects of college administration""",
+    'author': "Marcellius",
+    'category': "Education",
+    'website': "www.cybrosys.com",
+    'maintainer': "Marcellius Pt Holding <marcelliusfelixmatius@gmail.com>",
+    'sequence': 1,
+    'data': [
+        "views/college_student.xml",
+        "views/college_erp_menus.xml",
+    ],
+    'application': True,
+    'auto_install': False,
+    'installable': True
+}
