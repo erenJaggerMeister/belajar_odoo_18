38d7e0f4cba0a4dfa14567e4b3c8ea4322fa51e4
erenJaggerMeister <marcelliusfelixmatius@gmail.com>
2026-08-14 16:36:03 +0700

    2026/08/13 V2: penambahan create & add fields

--- odoo18/custom_addons/college_erp/models/college_student.py
+++ odoo18/custom_addons/college_erp/models/college_student.py
@@ -1,5 +1,22 @@
-from odoo import models
+from odoo import fields, models
 
 class CollegeStudent(models.Model):
     _name = "college.student"
     _description = "College Student"
+
+    admission_no = fields.Char(string="Admission Name", required=True)
+    admission_date = fields.Date(string="Admission Date", required=True)
+    first_name = fields.Char(string="First Name", required=True)
+    last_name = fields.Char(string="Last Name", required=True)
+    father_name = fields.Char(string="Father's Name",required=True)
+    mother_name = fields.Char(string="Mother's Name",required=True)
+    communication_address = fields.Text(string="Communication Address",required=True)
+    street = fields.Char()
+    street2 = fields.Char()
+    zip = fields.Char()
+    city = fields.Char()
+    state_id = fields.Many2one('res.country.state', 'Fed. State', domain="[('country_id', '=?', country_id)]")
+    country_id = fields.Many2one('res.country')
+    country_code = fields.Char(related='country_id.code', string='Country Code')
+    email = fields.Char(string="Email")
+    phone = fields.Char(string="Phone")
